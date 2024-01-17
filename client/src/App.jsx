c24882f512fe096c41de5133774db2a759dec4e6
laxmankusetti <laxmankusetti@gmail.com>
2024-01-17 09:55:45 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Profile from './pages/Profile.jsx';
+import SignIn from './pages/SignIn.jsx';
+import SignUp from './pages/SignUp.jsx';
+import About from './pages/About.jsx';
+import Header from './components/Header.jsx';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,40 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header className="bg-slate-300">
+      <div className="flex justify-between max-w-6xl m-auto p-3">
+        <h1 className="font-bold">
+          <span className="text-slate-500 text-xl sm:text-2xl">LAXMAN</span>
+          <span className="text-slate-700 text-xl">Estate</span>
+        </h1>
+        <form className="flex justify-between p-3 rounded-lg bg-slate-100 items-center">
+          <input
+            type="text"
+            placeholder="Search here..."
+            className="focus:outline-none bg-transparent"
+          />
+          <FaSearch className="text-slate-600 cursor-pointer" />
+        </form>
+        <ul className="flex gap-4 items-center">
+          <Link to="/">
+            <li className="hidden sm:inline hover:underline text-slate-700 cursor-pointer font-medium">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline hover:underline text-slate-700 cursor-pointer font-medium">
+              About
+            </li>
+          </Link>
+          <Link to="/sign-in">
+            <li className="hidden sm:inline hover:underline text-slate-700 cursor-pointer font-medium">
+              Sign In
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
