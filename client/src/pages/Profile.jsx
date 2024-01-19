736e367948bb013e9454dc93f52f8756e642597e
laxmankusetti <laxmankusetti@gmail.com>
2024-01-19 13:34:17 +0530

    completed image upload functionality in the profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,112 @@
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 export default function Profile() {
-  const currentUser = useSelector(state => state.user.currentUser)
+  const currentUser = useSelector((state) => state.user.currentUser);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+  const fileRef = useRef(null);
+  console.log(filePerc);
+  console.log(fileUploadError);
+  console.log(formData);
+  // allow read;
+  // allow write: if
+  // request.resource.size < 2 * 1024 * 1024 &&
+  // request.resource.contentType.matches('image/.*')
+  useEffect(() => {
+    if (file) {
+      handleUploadFile(file);
+    }
+  }, [file]);
+
+  const handleUploadFile = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(progress);
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
+      }
+    );
+  };
   return (
     <div className="max-w-lg mx-auto p-3">
       <h1 className="text-3xl text-center font-semibold my-5">Profile</h1>
       <form className="flex flex-col gap-4">
-        <img src={currentUser.avatar} alt="profile pic" className="w-24 h-24 rounded-full self-center"/>
-        <input type="text" id="username" placeholder="username" className="p-3 border rounded-lg focus:outline-none"/>
-        <input type="email" id="email" placeholder="email" className="p-3 border rounded-lg focus:outline-none"/>
-        <input type="password" id="password" placeholder="password" className="p-3 border rounded-lg focus:outline-none"/>
+        <input
+          type="file"
+          ref={fileRef}
+          hidden
+          onChange={(e) => {
+            setFile(e.target.files[0]);
+          }}
+        />
+        <img
+          src={ formData.avatar || currentUser.avatar }
+          alt="profile pic"
+          className="w-24 h-24 rounded-full self-center cursor-pointer"
+          onClick={() => {
+            fileRef.current.click();
+          }}
+          accept="image/*"
+        />
+        <p className="self-center font-sm">
+          {fileUploadError ? (
+            <span className="text-red-700">
+              An error accured when uploading image (image should be less than
+              2MB)
+            </span>
+          ) : filePerc > 0 && filePerc < 100 ? (
+            <span className="text-slate-600">{`uploading ${filePerc}%`}</span>
+          ) : filePerc === 100 ? (
+            <span className="text-green-600">Image uploaded successfully</span>
+          ) : (
+            ""
+          )}
+        </p>
+        <input
+          type="text"
+          id="username"
+          placeholder="username"
+          className="p-3 border rounded-lg focus:outline-none"
+        />
+        <input
+          type="email"
+          id="email"
+          placeholder="email"
+          className="p-3 border rounded-lg focus:outline-none"
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="password"
+          className="p-3 border rounded-lg focus:outline-none"
+        />
+        <button className="bg-green-600 text-white rounded-lg p-3 uppercase">
+          Update
+        </button>
       </form>
       <div className="flex justify-between mt-5">
         <span className="text-red-700 cursor-pointer font-semibold">
@@ -20,5 +117,5 @@ export default function Profile() {
         </span>
       </div>
     </div>
-  )
+  );
 }
