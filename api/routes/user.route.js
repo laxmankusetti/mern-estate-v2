ab81c90c6b03776efba0f0d81fc0550896f6b956
laxmankusetti <laxmankusetti@gmail.com>
2024-01-22 10:11:21 +0530

    created user listings api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { update, deleteUser } from '../controllers/user.controller.js';
+import { update, deleteUser, getUserListings } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/update/:id', verifyToken, update);
 router.delete('/delete/:id', verifyToken, deleteUser);
+router.get('/listings/:id', verifyToken, getUserListings);
 
 export default router;
